2d47ad5ba814b729fbe486039ab42e909c0e43b0
aiamu <zehmight@gmail.com>
2022-09-24 01:42:45 +0900

    add input interface

--- recipe_programms/src/index.tsx
+++ recipe_programms/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react';
+import { Button, ButtonGroup } from '@chakra-ui/react'
 import Tesseract from 'tesseract.js';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-
+    <Input placeholder="file" type="file"/>
+    <div><Button colorScheme='teal' size='md'>
+    Button
+  </Button></div>
   </React.StrictMode>
 );
 
