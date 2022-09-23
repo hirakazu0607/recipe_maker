8531fdc5b4f2cb18f66c47374b11b91c8a59ddc0
hirakazu <surakazu0607@docomo.ne.jp>
2022-09-24 01:22:19 +0900

    ちゃくら変更

--- recipe_programms/src/index.tsx
+++ recipe_programms/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import Tesseract from 'tesseract.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
