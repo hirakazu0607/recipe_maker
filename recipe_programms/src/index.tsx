61dd0dfc178516f6b42a498da629f0bf15d2cfcf
aiamu <zehmight@gmail.com>
2022-09-24 01:11:26 +0900

    import libraries

--- recipe_programms/src/index.tsx
+++ recipe_programms/src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import Tesseract from 'tesseract.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    
+
   </React.StrictMode>
 );
 
